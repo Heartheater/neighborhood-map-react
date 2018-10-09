e0c1ca2649a06f9e57ab0ef886fcfab77b58e3c9
cinder <meikopolis@gmail.com>
2018-10-09 16:09:18 -0700

    edited map and style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './Map';
+import {GoogleApiWrapper} from 'google-maps-react';
+import MapContainer from './Map';
 import ErrorHandler from './ErrorHandler';
+
+//google maps Api key
+//change this if you want to use a different api key
+const googleApiKey = `AIzaSyCvRrihVSVciSghCR1F0HLX1pQXwL_xmTM`;
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
             mapCenter: {
-                lat: 34.540309,
-                lng: -112.469661
+                lat: 34.7497933,
+                lng: -112.1151577
             }
         };
     }
@@ -21,12 +26,13 @@ class App extends Component {
         </header>
 
         <ErrorHandler>
-            <Map
+            <MapContainer
+                google={this.props.google}
                 initialCenter={{
                     lat: this.state.mapCenter.lat,
                     lng: this.state.mapCenter.lng
                 }}
-                defaultZoom={15}
+                defaultZoom={17}
             />
         </ErrorHandler>
       </main>
@@ -34,4 +40,9 @@ class App extends Component {
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+    //pass apiKey and google maps url to the api call's parameters
+    apiKey: googleApiKey,
+    url: 'https://maps.googleapis.com/maps/api/js',
+})(App);
+
--- src/Map.js
+++ src/Map.js
@@ -1,45 +1,71 @@
 import React, { Component } from 'react';
-import { Marker, Map, GoogleApiWrapper, InfoWindow } from 'google-maps-react';
+import ReactDOM from 'react-dom';
+const markerLocationsArray = [
+    { name: 'Haunted Hamburger', lat: 34.750263, lng: -112.11605 },
+    { name: 'Ghost City Inn', lat: 34.7489484, lng: -112.1145002 },
+    { name: 'Ghost Town Tours', lat: 34.7506988, lng: -112.116719 },
+    { name: 'Jerome Artists Co-op', lat: 34.7496591, lng: -112.114419 },
+    { name: 'Jerome Sliding Jail', lat: 34.7508657, lng: -112.1146598 },
+    { name: 'The Mine Cafe', lat: 34.7511169, lng: -112.1158929 },
+    { name: 'Mile High Grill & Inn', lat: 34.750431, lng: -112.1159191 },
+    { name: 'The Miners Pick Rock Shop', lat: 34.7493362, lng: -112.1155664 },
+    { name: 'Firefly Gallery Jerome', lat: 34.7508784, lng: -112.1158882 },
+    { name: 'Spirit Room', lat: 34.7512062, lng: -112.1162356 },
+    { name: 'The Flatiron', lat: 34.749793, lng: -112.114615 },
+    { name: 'Surgeon\'s House', lat: 34.749636, lng: -112.115789 }
+];
 
-//google maps Api key
-//change this if you want to use a different api key
-const googleApiKey = `AIzaSyCvRrihVSVciSghCR1F0HLX1pQXwL_xmTM`;
-
-export class MapContainer extends Component {
+export default class MapContainer extends Component {
     constructor() {
         super();
         this.state = {
-            mapCenter: {
+            defaultMapCenter: {
                 lat: 40.7413549,
                 lng: -73.9980244
             },
             windowOpen: false,
             activeMarker: null,
-            place: 'location name',
             allMarkers: []
         };
     }
 
+    componentDidMount() {
+        this.loadMap();
+    }
+
+    loadMap() {
+        //check if google is available
+        if (this.props && this.props.google) {
+            const { google } = this.props;
+
+            const mapContainer = this.refs.map;
+
+            //create map
+            const center = new google.maps.LatLng(this.props.initialCenter.lat, this.props.initialCenter.lng);
+            const mapConfig = Object.assign({}, {
+                center: center,
+                zoom: this.props.defaultZoom,
+            })
+            this.map = new google.maps.Map(mapContainer, mapConfig);
+        }
+    }
+
+    addMarkers = () => {
+
+
+        return;
+    }
 
     render() {
         return (
-            <Map
-                google={this.props.google}
-                initialCenter={this.props.initialCenter ? this.props.initialCenter : this.state.mapCenter}
-                zoom={this.props.defaultZoom ? this.props.defaultZoom : 13}
-                style={{
-                    width: '100%',
-                    height: '100%'
-                }}
-            >
-            </Map>
-        );
+            <div className="map-container"> 
+                <div className="map" ref="map" role="application">
+                    loading map...
+                </div>
+            </div>
+            );
     }
 }
 
 
-export default GoogleApiWrapper({
-    //pass apiKey and google maps url to the api call's parameters
-    apiKey: googleApiKey,
-    url: 'https://maps.googleapis.com/maps/api/js',
-})(MapContainer);
+
